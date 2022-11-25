f2764a526150460716f2c820a2398969481f0ab2
SIDANWhatever <wongkahinhinson@gmail.com>
2022-11-25 15:18:25 +0800

    fix: fix minor bugs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,53 +1,53 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'DeltaDefi Documentation',
-  tagline: 'Understanding the decentralized financial option protocol',
-  url: 'https://deltadefi.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "DeltaDeFi Documentation",
+  tagline: "Understanding the decentralized financial option protocol",
+  url: "https://deltadefi.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -57,87 +57,87 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'DeltaDeFi Docs',
+        title: "DeltaDeFi Docs",
         logo: {
-          alt: 'DeltaDeFi Logo',
-          src: 'img/deltadefi.png',
+          alt: "DeltaDeFi Logo",
+          src: "img/deltadefi.png",
         },
         items: [
           {
-            label: 'Guide',
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
+            label: "Guide",
+            type: "doc",
+            docId: "intro",
+            position: "left",
           },
           {
-            label: 'Whitepaper',
-            to: '/docs/category/whitepaper',
-            position: 'left',
+            label: "Whitepaper",
+            to: "/docs/category/whitepaper",
+            position: "left",
           },
           {
-            label: 'Security',
-            to: '/docs/category/security-and-audits',
-            position: 'left',
+            label: "Security",
+            to: "/docs/category/security-and-audits",
+            position: "left",
           },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            label: 'GitHub',
-            href: 'https://github.com/SIDANWhatever/DeltaDefi_Docs',
-            position: 'right',
+            label: "GitHub",
+            href: "https://github.com/SIDANWhatever/DeltaDefi_Docs",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Guide',
-                to: '/docs/intro',
+                label: "Guide",
+                to: "/docs/intro",
               },
               {
-                label: 'Whitepaper',
-                to: '/docs/category/whitepaper',
+                label: "Whitepaper",
+                to: "/docs/category/whitepaper",
               },
               {
-                label: 'Security',
-                to: '/docs/category/security-and-audits',
+                label: "Security",
+                to: "/docs/category/security-and-audits",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.gg/28VFwF49uS',
+                label: "Discord",
+                href: "https://discord.gg/28VFwF49uS",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/deltadefi',
+                label: "Twitter",
+                href: "https://twitter.com/deltadefi",
               },
               {
-                label: 'LinkedIn',
-                href: 'https://www.linkedin.com/',
+                label: "LinkedIn",
+                href: "https://www.linkedin.com/",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'DeltaDeFi',
-                to: 'https://deltadefi.io',
+                label: "DeltaDeFi",
+                to: "https://deltadefi.io",
               },
               {
-                label: 'SIDAN Studio',
-                to: 'https://sidan.io',
+                label: "SIDAN Studio",
+                to: "https://sidan.io",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/SIDANWhatever/DeltaDefi_Docs',
+                label: "GitHub",
+                href: "https://github.com/SIDANWhatever/DeltaDefi_Docs",
               },
             ],
           },
